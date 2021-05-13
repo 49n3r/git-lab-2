3b87123afc42520c07e021e1c6dffc5ba38ae148
Chinonso Ugwumadu <cu884120@ohio.edu>
2021-05-12 20:06:23 -0400

    first C++ code

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+   File:        git-lab-program.cc
+   Author:      UGWUMADU, Chinonso Ephraim
+   Date:        05/12/2021
+   Description: my first block of C++ codes for git 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}//main
